ogham/exa

Don’t show icons when piped (also add --icons={always,auto,never})

src/output/file_name.rs
@@ -18,7 +18,10 @@ pub struct Options {
     pub classify: Classify,
 
     /// Whether to prepend icon characters before file names.
-    pub show_icons: ShowIcons,
+    pub show_icons: UseIcons,
+
+    /// Whether we are in a console or redirecting the output
+    pub is_a_tty: bool,
 }
 
 impl Options {
@@ -74,14 +77,16 @@ impl Default for Classify {
 
 /// Whether and how to show icons.
 #[derive(PartialEq, Debug, Copy, Clone)]
-pub enum ShowIcons {
+pub enum UseIcons {
+    /// Display icons next to file names, with the given number of spaces between
+    /// the icon and the file name, even when output isn’t going to a terminal.
+    Always(u32),
 
-    /// Don’t show icons at all.
-    Off,
+    /// Same as Always, but only when output is going to a terminal, not otherwise.
+    Automatic(u32),
 
-    /// Show icons next to file names, with the given number of spaces between
-    /// the icon and the file name.
-    On(u32),
+    /// Never display them, even when output is going to a terminal.
+    Never,
 }
 
 
@@ -125,7 +130,13 @@ impl<'a, 'dir, C: Colours> FileName<'a, 'dir, C> {
     pub fn paint(&self) -> TextCellContents {
         let mut bits = Vec::new();
 
-        if let ShowIcons::On(spaces_count) = self.options.show_icons {
+        let spaces_count_opt = match self.options.show_icons {
+            UseIcons::Always(spaces_count) => Some(spaces_count),
+            UseIcons::Automatic(spaces_count) if self.options.is_a_tty => Some(spaces_count),
+            _ => None,
+        };
+
+        if let Some(spaces_count) = spaces_count_opt {
             let style = iconify_style(self.style());
             let file_icon = icon_for_file(self.file).to_string();
 
@@ -170,7 +181,8 @@ impl<'a, 'dir, C: Colours> FileName<'a, 'dir, C> {
                     if ! target.name.is_empty() {
                         let target_options = Options {
                             classify: Classify::JustFilenames,
-                            show_icons: ShowIcons::Off,
+                            show_icons: UseIcons::Never,
+                            is_a_tty: self.options.is_a_tty,
                         };
 
                         let target_name = FileName {

wlritchi/eza

src/output/file_name.rs
@@ -21,6 +21,9 @@ pub struct Options {
 
     /// Whether to make file names hyperlinks.
     pub embed_hyperlinks: EmbedHyperlinks,
+
+    /// Whether we are in a console or redirecting the output
+    pub is_a_tty: bool,
 }
 
 impl Options {
@@ -91,14 +94,17 @@ enum MountStyle {
 }
 
 /// Whether and how to show icons.
-#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+#[derive(PartialEq, Debug, Copy, Clone)]
 pub enum ShowIcons {
-    /// Don’t show icons at all.
-    Off,
+    /// Display icons next to file names, with the given number of spaces between
+    /// the icon and the file name, even when output isn’t going to a terminal.
+    Always(u32),
 
-    /// Show icons next to file names, with the given number of spaces between
-    /// the icon and the file name.
-    On(u32),
+    /// Same as Always, but only when output is going to a terminal, not otherwise.
+    Automatic(u32),
+
+    /// Never display them, even when output is going to a terminal.
+    Never,
 }
 
 /// Whether to embed hyperlinks.
@@ -162,7 +168,13 @@ impl<'a, 'dir, C: Colours> FileName<'a, 'dir, C> {
     pub fn paint(&self) -> TextCellContents {
         let mut bits = Vec::new();
 
-        if let ShowIcons::On(spaces_count) = self.options.show_icons {
+        let spaces_count_opt = match self.options.show_icons {
+            ShowIcons::Always(spaces_count) => Some(spaces_count),
+            ShowIcons::Automatic(spaces_count) if self.options.is_a_tty => Some(spaces_count),
+            _ => None,
+        };
+
+        if let Some(spaces_count) = spaces_count_opt {
             let style = iconify_style(self.style());
             let file_icon = icon_for_file(self.file).to_string();
 
@@ -207,8 +219,9 @@ impl<'a, 'dir, C: Colours> FileName<'a, 'dir, C> {
                     if !target.name.is_empty() {
                         let target_options = Options {
                             classify: Classify::JustFilenames,
-                            show_icons: ShowIcons::Off,
+                            show_icons: ShowIcons::Never,
                             embed_hyperlinks: EmbedHyperlinks::Off,
+                            is_a_tty: self.options.is_a_tty,
                         };
 
                         let target_name = FileName {